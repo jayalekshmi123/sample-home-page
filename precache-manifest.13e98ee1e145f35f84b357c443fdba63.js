8d4365fd09a34a8fbc58a1eb555e5d8f6e5c816e
jayalekshmi123 <jayalekshmirajeswarijoy@gmail.com>
2020-08-13 10:45:34 +0530

    Updates

---
+++ precache-manifest.13e98ee1e145f35f84b357c443fdba63.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c83a26c06db9e088f77cda8d8d086f",
+    "url": "/sample-home-page/index.html"
+  },
+  {
+    "revision": "276eb5fa68b35f10f4ef",
+    "url": "/sample-home-page/static/css/2.f94ba274.chunk.css"
+  },
+  {
+    "revision": "4e0feb3b22ff0110cdbf",
+    "url": "/sample-home-page/static/css/4.8c191d56.chunk.css"
+  },
+  {
+    "revision": "276eb5fa68b35f10f4ef",
+    "url": "/sample-home-page/static/js/2.4bcd5e36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sample-home-page/static/js/2.4bcd5e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ce772d8db663b97772",
+    "url": "/sample-home-page/static/js/3.c03cd09f.chunk.js"
+  },
+  {
+    "revision": "4e0feb3b22ff0110cdbf",
+    "url": "/sample-home-page/static/js/4.1f7906c1.chunk.js"
+  },
+  {
+    "revision": "fb8693ef1dac93d2636765e6778bc5af",
+    "url": "/sample-home-page/static/js/4.1f7906c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac54adab877b0a50fde",
+    "url": "/sample-home-page/static/js/main.432e375f.chunk.js"
+  },
+  {
+    "revision": "8d75506797934fcbe072",
+    "url": "/sample-home-page/static/js/runtime-main.ef46f4c8.js"
+  },
+  {
+    "revision": "0ed4c66b81b4951f07f1a172bad8ce22",
+    "url": "/sample-home-page/static/media/aashirvaad.0ed4c66b.jpg"
+  },
+  {
+    "revision": "ba3daf3548bb8f9153fb07e73403d72b",
+    "url": "/sample-home-page/static/media/amul.ba3daf35.jpg"
+  },
+  {
+    "revision": "b471c6241ea69563ecbe085c83a6c0d6",
+    "url": "/sample-home-page/static/media/bg_1.b471c624.jpg"
+  },
+  {
+    "revision": "f576abeabed8affc4d2e93254e386fcf",
+    "url": "/sample-home-page/static/media/bg_2.f576abea.jpg"
+  },
+  {
+    "revision": "e21add4b01e7e817cfb8318640381a9d",
+    "url": "/sample-home-page/static/media/dove.e21add4b.jpg"
+  },
+  {
+    "revision": "9afc19a69c48f03b312346299c98e2af",
+    "url": "/sample-home-page/static/media/indiagate.9afc19a6.jpg"
+  },
+  {
+    "revision": "e9303a60213c6441771efe52bfbe14e5",
+    "url": "/sample-home-page/static/media/lifeboy.e9303a60.jpg"
+  },
+  {
+    "revision": "657f660372a9a39c39acfdac135dc3cb",
+    "url": "/sample-home-page/static/media/logo-white.657f6603.png"
+  },
+  {
+    "revision": "4690ce91e0a2124806fa2265fcd367ab",
+    "url": "/sample-home-page/static/media/logo.4690ce91.png"
+  },
+  {
+    "revision": "e0f7eb56ada57e7cb58325c152ce1cec",
+    "url": "/sample-home-page/static/media/mango.e0f7eb56.png"
+  },
+  {
+    "revision": "d4e468e1545a771172ec671837585b48",
+    "url": "/sample-home-page/static/media/nirapara.d4e468e1.jpg"
+  },
+  {
+    "revision": "01b06c920ffffe753b53bcabb5233989",
+    "url": "/sample-home-page/static/media/organic.01b06c92.png"
+  },
+  {
+    "revision": "35d992542a5dd106de63c78fb177e870",
+    "url": "/sample-home-page/static/media/product01.35d99254.png"
+  },
+  {
+    "revision": "51469396d0a3cca8e9f279867bd1ad64",
+    "url": "/sample-home-page/static/media/product02.51469396.png"
+  },
+  {
+    "revision": "f9e432b3d0027daa475a9e432b5195b2",
+    "url": "/sample-home-page/static/media/product03.f9e432b3.png"
+  },
+  {
+    "revision": "b1a7988d81549124acc49e2297bf522c",
+    "url": "/sample-home-page/static/media/product04.b1a7988d.png"
+  },
+  {
+    "revision": "8a5ba653dc6fe3c18873c9465a3a91e2",
+    "url": "/sample-home-page/static/media/product05.8a5ba653.png"
+  },
+  {
+    "revision": "329d43da800c1b1ff572d3f5880ed9fa",
+    "url": "/sample-home-page/static/media/product06.329d43da.png"
+  },
+  {
+    "revision": "9ed703cec8ac613252226ca00a059946",
+    "url": "/sample-home-page/static/media/product07.9ed703ce.png"
+  },
+  {
+    "revision": "c5b3549bd6d9e70c2e531a524bbe306b",
+    "url": "/sample-home-page/static/media/product08.c5b3549b.png"
+  },
+  {
+    "revision": "30023151180ec526d675ef6ee49a51bd",
+    "url": "/sample-home-page/static/media/product09.30023151.png"
+  }
+]);
